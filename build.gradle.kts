589960c2aad8cdacdb8f49717fbf150d271fd468
Gabriel Konat <gabrielkonat@gmail.com>
2019-06-04 14:00:06 +0200

    Bootstrap

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
   id("org.metaborg.gradle.config.root-project") version "0.3.7"
-  id("org.metaborg.gitonium") version "0.1.1" // Bootstrap with previous version.
+  id("org.metaborg.gitonium") version "0.1.2" // Bootstrap with previous version.
   kotlin("jvm") version "1.3.21"
   `kotlin-dsl`
   `java-gradle-plugin`
