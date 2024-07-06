0a64c87b58e7e9373a58a1878dd3ea381a971e74
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-07-06 22:32:24 +0200

    Use version catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,17 @@ pluginManagement {
     }
 }
 
+dependencyResolutionManagement {
+    repositories {
+        maven("https://artifacts.metaborg.org/content/groups/public/")
+    }
+    versionCatalogs {
+        create("libs") {
+            from("org.metaborg.spoofax3:catalog:0.3.4")
+        }
+    }
+}
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
